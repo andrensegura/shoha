3e2fb7a9d0dfa78219974c286f005589bd107563
Andre Segura <andrensegura@gmail.com>
2021-10-29 15:17:30 -0700

    more responses

--- discord/triggered_responses.py
+++ discord/triggered_responses.py
@@ -3,7 +3,7 @@ import random
 responses = {
     "steve jobs": ["who the hell is steve jobs"],
     "pogger": ["> pogger\ncringe"],
-    "ligma balls": ["https://i.imgur.com/HPduAKn.gif","https://tenor.com/view/ligma-balls-gif-22082587","https://tenor.com/view/dead-i-am-dead-ded-terminator-terminator2-gif-5663820"]
+    "ligma balls": ["https://i.imgur.com/HPduAKn.gif","https://tenor.com/view/ligma-balls-gif-22082587","https://tenor.com/view/dead-i-am-dead-ded-terminator-terminator2-gif-5663820","https://i.imgur.com/1PG4GEC.gif","https://c.tenor.com/gMACtWvF0v0AAAAC/pissed-angry.gif","https://thinkinganimation.com/wp-content/uploads/2013/06/tumblr_mowgalVS3O1snfsquo1_400.gif","https://media1.giphy.com/media/djTw5269awMtW/200.gif"]
 }
 
 def process(msg):
