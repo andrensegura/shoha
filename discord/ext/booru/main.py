457abbe0b07036d47616fe209d03966cf4905dd5
Andre Segura <andrensegura@gmail.com>
2021-10-31 18:25:04 -0700

    added rating

--- discord/ext/booru/main.py
+++ discord/ext/booru/main.py
@@ -3,32 +3,46 @@ from discord import Embed
 from discord.ext import commands
 import sys
 
-client = Danbooru('danbooru')
-
 class Booru(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.client = Danbooru('danbooru')
 
-    def get_post(self, search_tag):
+    def get_post(self, search_tag, search_rating='s'):
+        # search_rating = s, e, q
+        client = Danbooru('danbooru')
         results = client.post_list(limit=1, tags=search_tag, random=True)
         result = results[0]
+        while result['rating'] != search_rating:
+            results = client.post_list(limit=1, tags=search_tag, random=True)
+            result = results[0]
+        print(result)
         page_link = "https://danbooru.donmai.us/posts/" + str(result['id'])
         image_link = result['file_url']
         return (page_link, image_link)
 
     @commands.command()
-    async def booru(self, ctx, tag: str):
+    async def booru(self, ctx, tag: str, nsfw: str):
         if not tag:
             await ctx.send("Need a tag.\n`shoha booru \"tag_here\"`")
             return
-        
-        post = self.get_post(tag)
-        embed=Embed(title="random " + tag, color=0xfdf6e6)
-        embed.add_field(name="Post URL:", inline=False)
-        embed.add_field(value=post[0], inline=False)
-        embed.set_image(url=post[1])
-        return await ctx.send(embed=embed)
+
+        if nsfw == 'nsfw':
+            nsfw = 'e'
+            r_color = 0xff0000
+        elif nsfw == 'questionable':
+            nsfw = 'q'
+            r_color = 0x0000ff
+        else:
+            nsfw = 's'
+            r_color = 0x00ff00
+
+        async with ctx.channel.typing():
+            post = self.get_post(tag, nsfw)
+            embed=Embed(title="random " + tag, color=r_color)
+            embed.add_field(name="Post URL:", value=post[0], inline=False)
+            embed.set_image(url=post[1])
+        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(Booru(bot))
